f50e5a2de1893e68ffb06c1882d338dd55ed8e91
Ilikepizza2 <lit2021010@iiitl.ac.in>
2022-04-23 05:08:29 +0530

    backend

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,44 @@
-const express = require('express');
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+
+import LOGIN_ROUTE from "./Routes/login.js";
+import REGISTER_ROUTE from "./Routes/register.js";
+
 const app = express();
-const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Why are you here');
-})
+app.use(
+  bodyParser.urlencoded({
+    limit: "30mb",
+    extended: true,
+  })
+);
+app.use(
+  bodyParser.json({
+    limit: "30mb",
+    extended: true,
+  })
+);
+app.use(cors());
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-})
+const MONGO_URI =
+  "mongodb+srv://Ilikepizza2:Lushcats@lushcats.brhag.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server up and running on http://localhost:${PORT}/`);
+    });
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+app.use("/", LOGIN_ROUTE);
+app.use("/", REGISTER_ROUTE);
