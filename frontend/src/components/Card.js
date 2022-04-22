3167ecfd102c0540b2f501bd479ade78952ec7f3
Prince Singh <marvelousprince012233@gmail.com>
2022-04-23 00:36:18 +0530

    minor fix

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -5,15 +5,15 @@ import upvote from '../assets/upvote.png'
 
 export const Card = () => {
   return (
-    <div class="card">
+    <div className="card">
       {/* TODO: 1. LINK PROFILE PHOTO AND NAME FROM DATABASE 2. MAKE STUFF CLICKABLE */}
-      <div class="nameAndProfile"><img src={user} alt="user.png" width="30px" /><span>Name</span></div>
-      <div class="card-content"></div>
-      <div class="card-body">
+      <div className="nameAndProfile"><img src={user} alt="user.png" width="30px" /><span>Name</span></div>
+      <div className="card-content"></div>
+      <div className="card-body">
         <p>Lorem ipsum dolor sit amet.</p>
       </div>
-      <div class="votesAndComments">
-        <a href='/'><img src={upvote} alt="" width="14px" class="upvoteImage" /></a><span class="upvoteCount">0</span>
+      <div className="votesAndComments">
+        <a href='/'><img src={upvote} alt="" width="14px" className="upvoteImage" /></a><span className="upvoteCount">0</span>
       </div>
     </div>
   )
