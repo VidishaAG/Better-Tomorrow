9d015a8880ffd4d00d24915cea2ebbb336c1bd06
Ilikepizza2 <lit2021010@iiitl.ac.in>
2022-04-23 07:55:04 +0530

    backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,20 @@
 import './App.css';
 import './css/home.css'
 import './css/login.css'
+import './css/about.css'
 
 import { Navbar } from './components/Navbar';
 import { Card } from './components/Card';
 import { Login } from './components/Login';
+import { About } from './components/About';
 
 function App() {
   return (
     <div>
-      <Navbar />
-      <Card />
-      <Login />
+      {/* <Navbar /> */}
+      {/* <Card /> */}
+      {/* <Login /> */}
+      <About />
     </div>
   );
 }
