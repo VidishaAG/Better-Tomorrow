440447f4c45f79eca87a176a3202ed0a9c335b8d
Saarthak Verma <saarthakverma739@gmail.com>
2022-04-23 14:44:22 +0530

    Issues progress

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -1,19 +1,31 @@
-import React from 'react'
-
+import {React,useState} from 'react'
 import user from '../assets/user.png'
 import upvote from '../assets/upvote.png'
-
 export const Card = () => {
+  const [counter,setCounter] = useState(0);
+  const [x,setX] = useState(0);
+  const fun = () => {
+    if(x===1){
+      setX(0);
+      console.log(x);
+      setCounter( count => count - 1);
+    }
+    else if(x===0){
+      setX(1);
+      setCounter( count => count + 1);
+    }
+
+  }
   return (
     <div className="card">
       {/* TODO: 1. LINK PROFILE PHOTO AND NAME FROM DATABASE 2. MAKE STUFF CLICKABLE */}
       <div className="nameAndProfile"><img src={user} alt="user.png" width="30px" /><span>Name</span></div>
-      <div className="card-content"></div>
-      <div className="card-body">
-        <p>Lorem ipsum dolor sit amet.</p>
+      <div className="title"><h2>Title</h2></div>
+      <div className="cardBody">
+        <p>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia ratione quos quae quo aliquam, beatae ducimus deserunt voluptatum eaque facere impedit quas ipsam cum vero velit deleniti numquam sapiente sunt magnam iure. Vel libero veritatis aliquam laudantium id laborum consequuntur est dolorum? Facere, doloribus repellat! Maxime, eius amet. Vero, libero.</p>
       </div>
       <div className="votesAndComments">
-        <a href='/'><img src={upvote} alt="" width="14px" className="upvoteImage" /></a><span className="upvoteCount">0</span>
+        <input type="image" src={upvote} alt="upvote.png" className="upvoteImage" style={{width:"14px"}} onClick={fun} /><span className="upvoteCount">{counter}</span>
       </div>
     </div>
   )
