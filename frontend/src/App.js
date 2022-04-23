f3b350a08a9f0293990ed65703dbf6f65103dc57
Saarthak Verma <saarthakverma739@gmail.com>
2022-04-23 08:51:28 +0530

    payment progress

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,14 +7,15 @@ import { Navbar } from './components/Navbar';
 import { Card } from './components/Card';
 import { Login } from './components/Login';
 import { About } from './components/About';
-
+import Payment from './components/Payment'
 function App() {
   return (
     <div>
       {/* <Navbar /> */}
       {/* <Card /> */}
       {/* <Login /> */}
-      <About />
+      {/* <About /> */}
+      <Payment />
     </div>
   );
 }
